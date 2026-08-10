6bfe2652186a6328df8871d5f16c094137d98e6e
automindiasac-netizen <automind.iasac@gmail.com>
2026-08-10 02:52:58 -0300

    Add files via upload

---
+++ trico/_next/static/GLwFGgU07zBgMQyhm4Hpk/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Novo-teste-de-site-jos-carlos-tricologista//_next/:path+",
+        "destination": "/Novo-teste-de-site-jos-carlos-tricologista/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
